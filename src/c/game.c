6b2776bd03243ab14aed28f067c52fe064202745
wwwnsai <65011588@kmitl.ac.th>
2023-10-07 22:50:32 +0700

    ADD: add blocks to place avatars

--- src/c/game.c
+++ src/c/game.c
@@ -6,6 +6,7 @@
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
 #define NUM_FRAMES 3
+#define NUM_BLOCKS 15
 
 // Function prototypes
 Vector2 get_center(Texture2D texture);
@@ -31,15 +32,20 @@ int currentFrame = 0;
 float frameTimer = 0;
 float frameSpeed = 0.3f;
 Vector2 imagePosition = {0, 0};
+Vector2 targetPositions[NUM_BLOCKS];
 Vector2 targetPosition = {0, 0};
 Vector2 targetPosition2 = {0, 0};
 Color brownColor_Laika = BROWN;
 Color brownColor_MC = BROWN;
 bool shouldCopyLaika = false;
 bool shouldCopyMC = false;
-bool block_empty[2] = {true, true};
+bool block_empty[NUM_BLOCKS];
+bool block_contains_Laika_animation[NUM_BLOCKS];
+bool block_contains_MC_animation[NUM_BLOCKS];
+bool shouldDrawAnimationLaika[NUM_BLOCKS] = {false};
 bool shouldDrawAnimationLaika1 = false;
 bool shouldDrawAnimationLaika2 = false;
+bool shouldDrawAnimationMC[NUM_BLOCKS] = {false};
 bool shouldDrawAnimationMC1 = false;
 bool shouldDrawAnimationMC2 = false;
 
@@ -114,6 +120,18 @@ void InitializeGame(void)
   Laika1 = LoadTexture("assets/images/avatar/Laika/Laika1.png");
   MegaChonker1 = LoadTexture("assets/images/avatar/MegaChonker/MegaChonker1.png");
 
+  // Set initial positions for the blocks
+  Vector2 blockStart = get_center(bg_yard);
+  for (int i = 0; i < NUM_BLOCKS; i++)
+  {
+    int col = i / 3; // Columns (0 to 4)
+    int row = i % 3; // Rows (0 to 2)
+    targetPositions[i] = (Vector2){blockStart.x + (col * 250), blockStart.y + (row * 250)};
+    block_empty[i] = true;
+    block_contains_Laika_animation[i] = false;
+    block_contains_MC_animation[i] = false;
+  }
+
   // Set initial positions
   targetPosition = get_center(bg_yard);
   targetPosition2 = (Vector2){get_center(bg_yard).x, get_center(bg_yard).y + 250};
@@ -133,64 +151,68 @@ void UpdateGame(void)
 
   if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
   {
-    if (IsMouseOverBox(mousePosition, (Vector2){0, 0}, Laika1))
+    for (int i = 0; i < NUM_BLOCKS; i++)
     {
-      shouldCopyLaika = true;
-      shouldCopyMC = false;
-      brownColor_Laika = DARKBROWN;
-      brownColor_MC = BROWN;
-    }
-    else if (IsMouseOverBox(mousePosition, (Vector2){0, 250}, MegaChonker1))
-    {
-      shouldCopyLaika = false;
-      shouldCopyMC = true;
-      brownColor_Laika = BROWN;
-      brownColor_MC = DARKBROWN;
-    }
-    else if (CheckCollisionPointRec(mousePosition, (Rectangle){targetPosition.x, targetPosition.y, 250, 250}))
-    {
-      if (shouldCopyLaika && block_empty[0])
+      if (IsMouseOverBox(mousePosition, (Vector2){0, 0}, Laika1))
       {
-        CopyImage(&imagePosition, targetPosition, Laika1);
-        shouldCopyLaika = false;
-        shouldDrawAnimationLaika1 = true;
-        brownColor_Laika = BROWN;
-        block_empty[0] = false;
-      }
-      else if (shouldCopyMC && block_empty[0])
-      {
-        CopyImage(&imagePosition, targetPosition, MegaChonker1);
+        shouldCopyLaika = true;
         shouldCopyMC = false;
-        shouldDrawAnimationMC1 = true;
+        brownColor_Laika = DARKBROWN;
         brownColor_MC = BROWN;
-        block_empty[0] = false;
+        printf("Block %d is %s\n", i, block_empty[i] ? "empty" : "occupied");
       }
-    }
-    else if (CheckCollisionPointRec(mousePosition, (Rectangle){targetPosition2.x, targetPosition2.y, 250, 250}))
-    {
-      if (shouldCopyLaika && block_empty[1])
+      else if (IsMouseOverBox(mousePosition, (Vector2){0, 250}, MegaChonker1))
       {
-        CopyImage(&imagePosition, targetPosition2, Laika1);
         shouldCopyLaika = false;
-        shouldDrawAnimationLaika2 = true;
+        shouldCopyMC = true;
         brownColor_Laika = BROWN;
-        block_empty[1] = false;
+        brownColor_MC = DARKBROWN;
+        printf("Block %d is %s\n", i, block_empty[i] ? "empty" : "occupied");
       }
-      else if (shouldCopyMC && block_empty[1])
+
+      else if (CheckCollisionPointRec(mousePosition, (Rectangle){targetPositions[i].x, targetPositions[i].y, 250, 250}))
       {
-        CopyImage(&imagePosition, targetPosition2, MegaChonker1);
-        shouldCopyMC = false;
-        shouldDrawAnimationMC2 = true;
-        brownColor_MC = BROWN;
-        block_empty[1] = false;
+        printf("=====================================\n");
+        if (shouldCopyLaika && block_empty[i])
+        {
+          CopyImage(&imagePosition, targetPositions[i], Laika1);
+          shouldDrawAnimationLaika[i] = true;
+          shouldDrawAnimationMC[i] = false;
+          brownColor_Laika = BROWN;
+          block_empty[i] = false;
+          block_contains_Laika_animation[i] = true; // Mark this block as containing animation
+          shouldCopyLaika = false;
+        }
+        else if (shouldCopyMC && block_empty[i])
+        {
+          CopyImage(&imagePosition, targetPositions[i], MegaChonker1);
+          shouldDrawAnimationLaika[i] = false;
+          shouldDrawAnimationMC[i] = true;
+          brownColor_MC = BROWN;
+          block_empty[i] = false;
+          block_contains_MC_animation[i] = true; // Mark this block as containing animation
+          shouldCopyMC = false;
+        }
+        else if (block_contains_Laika_animation[i]) // Check if animation is present
+        {
+          shouldDrawAnimationLaika[i] = true; // Re-draw the animation
+          shouldDrawAnimationMC[i] = false;
+        }
+        else if (block_contains_MC_animation[i]) // Check if animation is present
+        {
+          shouldDrawAnimationLaika[i] = false; // Re-draw the animation
+          shouldDrawAnimationMC[i] = true;
+        }
+        else
+        {
+          shouldDrawAnimationLaika[i] = false;
+          shouldDrawAnimationMC[i] = false;
+          shouldCopyLaika = false;
+          shouldCopyMC = false;
+          brownColor_Laika = BROWN;
+          brownColor_MC = BROWN;
+        }
       }
-    }
-    else
-    {
-      shouldCopyLaika = false;
-      shouldCopyMC = false;
-      brownColor_Laika = BROWN;
-      brownColor_MC = BROWN;
     }
   }
 }
@@ -205,31 +227,22 @@ void DrawGame(void)
 
   DrawRectangle(0, 0, 250, 250, brownColor_Laika);
   DrawRectangle(0, 250, 250, 250, brownColor_MC);
-  DrawRectangle(targetPosition.x, targetPosition.y, 250, 250, (Color){0, 255, 0, 0});
-  DrawRectangle(targetPosition2.x, targetPosition2.y, 250, 250, (Color){0, 255, 0, 0});
+  for (int i = 0; i < NUM_BLOCKS; i++)
+  {
+    DrawRectangle(targetPositions[i].x, targetPositions[i].y, 250, 250, (Color){0, 255, 0, 0});
 
-  if (shouldDrawAnimationLaika1)
-  {
-    animation(LaikaFrames, currentFrame, frameTimer, Laika, targetPosition);
-  }
-  if (shouldDrawAnimationMC1)
-  {
-    animation(MegaChonkerFrames, currentFrame, frameTimer, MegaChonker, targetPosition);
-  }
-  if (shouldDrawAnimationLaika2)
-  {
-    animation(LaikaFrames, currentFrame, frameTimer, Laika, targetPosition2);
-  }
-  if (shouldDrawAnimationMC2)
-  {
-    animation(MegaChonkerFrames, currentFrame, frameTimer, MegaChonker, targetPosition2);
-  }
-  else
-  {
-    DrawTexture(LaikaFrames[0], 0, 0, WHITE);
-    DrawTexture(MegaChonkerFrames[0], 0, 250, WHITE);
+    if (shouldDrawAnimationLaika[i])
+    {
+      animation(LaikaFrames, currentFrame, frameTimer, Laika, targetPositions[i]);
+    }
+
+    if (shouldDrawAnimationMC[i])
+    {
+      animation(MegaChonkerFrames, currentFrame, frameTimer, MegaChonker, targetPositions[i]);
+    }
   }
 
+  // Draw Laika1 and MegaChonker1 here (outside the loop)
   DrawTexture(Laika1, 0, 0, WHITE);
   DrawTexture(MegaChonker1, 0, 250, WHITE);
 
