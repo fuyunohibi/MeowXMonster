a4e3469d547836abdd783d140e6aeb48a4dd025f
Chawalya Mattayanpol <chawalya.mp@gmail.com>
2023-10-09 15:51:05 +0700

    Update game.asm

--- src/asm/game.asm
+++ src/asm/game.asm
@@ -1,7 +1,40 @@
-global add_integers
+.data
 
-section .text
-add_integers:
-    add rdi, rsi
-    mov rax, rdi ; result is returned in rax
-    ret
+.text
+; .global main
+.global reduce_HP
+.global score_kill
+.global check_price
+
+reduce_HP:
+    SUB R0, R0, R1      ;R0 current HP, R1 attack HP
+    BX LR
+
+score_kill:
+    ADD R0, R0, #1
+    BX LR
+
+check_price:
+    CMP R0, R1          ;R0 current money, cat price
+    BGT return_true
+    return_false
+
+return_true:
+    MOV R0, #1
+    BX LR
+
+return_false:
+    MOV R0, #0
+    BX LR
+
+
+
+
+
+
+ 
+
+
+
+
+    
