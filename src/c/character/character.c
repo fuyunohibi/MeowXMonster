3d12b0d812e09a20b9f53ffd718c484476f4800b
Ko-Kwan Mongkholtham <kokwan.m@gmail.com>
2023-10-09 17:36:27 +0700

    ADD: Change Price,HP, and display character.price

--- src/c/character/character.c
+++ src/c/character/character.c
@@ -3,17 +3,17 @@
 Character Laika = {
   .name = "Laika",
   .price = 100,
-  .HP = 100,
+  .HP = 150,
   .isAlive = true,
-  .attackDamage = 10,
-  .attackPerSecond = 1.5,
+  .attackDamage = 50,
+  .attackPerSecond = 0.4,
 };
 
 Character MegaChonker = {
   .name = "MegaChonker",
   .price = 350,
-  .HP = 150,
+  .HP = 400,
   .isAlive = true,
-  .attackDamage = 15,
-  .attackPerSecond = 1.0,
+  .attackDamage = 0,
+  .attackPerSecond = 0.0,
 };
