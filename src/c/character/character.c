5b03cc75330b3c27b2039f5854d19a61e427db77
pcr9x <petchppw@gmail.com>
2023-10-10 16:58:51 +0700

    Add: Monster struct

--- src/c/character/character.c
+++ src/c/character/character.c
@@ -1,29 +1,18 @@
 #include "../constant/game_constants.h"
 
-// ==================== PLAYABLE CHARACTERS ====================
-
 Character Laika = {
     .name = "Laika",
     .price = 100,
-    .HP = 200,
+    .HP = 150,
     .isAlive = true,
-    .attackDamage = 150,
-    .attackPerSecond = 0.0,
-};
-
-Character FartCat = {
-    .name = "FartCat",
-    .price = 250,
-    .HP = 300,
-    .isAlive = true,
-    .attackDamage = 250,
-    .attackPerSecond = 0.0,
+    .attackDamage = 50,
+    .attackPerSecond = 0.4,
 };
 
 Character MegaChonker = {
     .name = "MegaChonker",
-    .price = 200,
-    .HP = 500,
+    .price = 150,
+    .HP = 400,
     .isAlive = true,
     .attackDamage = 0,
     .attackPerSecond = 0.0,
@@ -34,40 +23,24 @@ Character Bomb = {
     .price = 400,
     .HP = 100,
     .isAlive = true,
-    .attackDamage = 350,
+    .attackDamage = 200,
     .attackPerSecond = 0.0,
 };
 
-
-
-// ==================== MONSTER CHARACTERS ====================
-
-MonsterCharacter Jelly = {
-    .name = "Jelly The Monster",
-    .killReward = 100,
+Character FartCat = {
+    .name = "FartCat",
+    .price = 300,
     .HP = 150,
-    .rateOfSpawn = 0.0,
     .isAlive = true,
     .attackDamage = 100,
     .attackPerSecond = 0.0,
 };
 
-MonsterCharacter Muscle = {
-    .name = "Muscle The Monster",
-    .killReward = 300,
+Character Jelly = {
+    .name = "Jelly the Monster",
+    .price = ,
     .HP = 150,
-    .rateOfSpawn = 0.0,
     .isAlive = true,
     .attackDamage = 100,
     .attackPerSecond = 0.0,
-};
-
-MonsterCharacter Ufo = {
-    .name = "Ufo The Monster",
-    .killReward = 500,
-    .HP = 150,
-    .rateOfSpawn = 0.0, 
-    .isAlive = true,
-    .attackDamage = 100,
-    .attackPerSecond = 0.0,
-};
+}
