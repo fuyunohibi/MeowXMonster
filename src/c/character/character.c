5af4f46fc6fc74ac0e5661f62598423de56545a9
wwwnsai <65011588@kmitl.ac.th>
2023-10-10 11:05:35 +0700

    ADD: cats

--- src/c/character/character.c
+++ src/c/character/character.c
@@ -1,19 +1,37 @@
 #include "../constant/game_constants.h"
 
 Character Laika = {
-  .name = "Laika",
-  .price = 100,
-  .HP = 150,
-  .isAlive = true,
-  .attackDamage = 50,
-  .attackPerSecond = 0.4,
+    .name = "Laika",
+    .price = 100,
+    .HP = 150,
+    .isAlive = true,
+    .attackDamage = 50,
+    .attackPerSecond = 0.4,
 };
 
 Character MegaChonker = {
-  .name = "MegaChonker",
-  .price = 350,
-  .HP = 400,
-  .isAlive = true,
-  .attackDamage = 0,
-  .attackPerSecond = 0.0,
+    .name = "MegaChonker",
+    .price = 150,
+    .HP = 400,
+    .isAlive = true,
+    .attackDamage = 0,
+    .attackPerSecond = 0.0,
+};
+
+Character Bomb = {
+    .name = "Bomb",
+    .price = 400,
+    .HP = 100,
+    .isAlive = true,
+    .attackDamage = 200,
+    .attackPerSecond = 0.0,
+};
+
+Character FartCat = {
+    .name = "FartCat",
+    .price = 300,
+    .HP = 150,
+    .isAlive = true,
+    .attackDamage = 100,
+    .attackPerSecond = 0.0,
 };
