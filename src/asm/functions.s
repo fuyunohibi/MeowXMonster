574ee14e217541100bc0d0edaa725a59a491cd48
premecwy <chawalya.mp@gmail.com>
2023-10-24 22:45:22 +0700

    add: assembly array

--- src/asm/functions.s
+++ src/asm/functions.s
@@ -1,17 +1,67 @@
-.bss                     ; section for uninitialized data
-    character_array: .skip 88                 ; reserve space for 4 doubles (4*8 = 32 bytes)
+.data                     ; section for uninitialized data
+    laika_array:    .double 1.0
+                    .double 100.0
+                    .double 150000.0
+                    .double 1.0
+                    .double 50000.0
+                    .double 0.5
+                    .double 180.0
+                    .double 0.0
+
+    fartcat_array:  .double 2.0
+                    .double 250.0
+                    .double 300000.0
+                    .double 1.0
+                    .double 150.0
+                    .double 0.05
+                    .double 0.0
+                    .double 0.0
+
+    megachonker_array   .double 3.0
+                        .double 200.0
+                        .double 400000.0
+                        .double 1.0
+                        .double 0.0
+                        .double 0.0
+                        .double 180.0
+                        .double 0.0
+
+    bomb_array:     .double 4.0
+                    .double 400.0
+                    .double 1.0
+                    .double 1.0
+                    .double 500000.0
+                    .double 0.0
+                    .double 180.0
+                    .double 0.0
 
     monster_array:  .skip 120
+    
+laika:
+    ldr r0, =laika_array
+    bx lr
 
+fartcat:
+    ldr r0, =fartcat_array
+    bx lr
+
+megachonker:
+    ldr r0, =megachonker_array
+    bx lr
+
+bomb:
+    ldr r0, =bomb_array
+    bx lr
 
 create_character:
-    push {r8,r9,r10,lr}
-    ldr r8 ,=character_array
-    cmp r8, #1
+    cmp r0, #1
+    beq laika
 
+    cmp r0, #2
+    beq fartcat
 
-isLaika:
-    
+    cmp r0, #3
+    beq megachonker
 
-
-
+    cmp r0, #4
+    beq bomb
